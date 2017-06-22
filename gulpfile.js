fa91d2a2beeb8b4f374cbdd494306c0e474c55ee
MAZA2 <mahmoud.zakria@mondiamedia.com>
2017-06-22 15:18:52 +0200

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,6 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var autoprefixer = require('gulp-autoprefixer')
 var connect = require('gulp-connect');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var jshint = require('gulp-jshint');
-var notificator = require('gulp-jshint-notify-reporter');
 var csso = require('gulp-csso');
 
 //sass-en
@@ -23,7 +19,6 @@ gulp.task('sass', function () {
 gulp.task('watch',function(){
 	gulp.watch('./src/sass/*.sass',['sass'])
 	gulp.watch('./public/**/*.html',['html'])
-	gulp.watch('./src/js/script.js',['browserify'])
 })
 
 
@@ -34,25 +29,6 @@ gulp.task('html', function() {
 
 })
 
-//Browserify
-gulp.task('browserify', function() {
-    return browserify('./src/js/script.js')
-        .bundle()
-        //Pass desired output filename to vinyl-source-stream
-        .pipe(source('script.js'))
-        // Start piping stream to tasks!
-        .pipe(gulp.dest('./public/js/'))
-        .pipe(connect.reload());
-});
-
-//Js lint
-gulp.task('lint', function() {
-  return gulp.src('./src/js/*.js')
-    .pipe(jshint())
-    .pipe(notificator())
-});
-
-//Localhost 
 gulp.task('server',function(){
 	connect.server({
 		root: 'public',
@@ -69,4 +45,4 @@ gulp.task('mini-css', function () {
 
 
 //default
-gulp.task('default',['watch','server','browserify','sass','mini-css'])
+gulp.task('default',['watch','server','sass','mini-css'])
